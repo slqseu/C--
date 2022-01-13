e87ec1ea7e1831ac3ee0a3ecef2bd9cbcddd6454
slqseu <slq13072020073@126.com>
2022-01-13 18:32:00 +0800

    11111

--- test/test.cpp
+++ test/test.cpp
@@ -1,5 +1,19 @@
 #include<iostream>
+#include<vector>
+#include<unordered_set>
+#include<unordered_map>
 using namespace std;
+
+
+template<class T> class MyStack
+{
+public:
+    MyStack(){};
+    virtual void Push(const T& x) = 0;
+    
+};
+
+
 
 int main(){
     cout<<"hello world"<<endl;
